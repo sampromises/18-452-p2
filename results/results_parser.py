a93124a05c290cf20f2ffa06658359aaf5fc8c45
Sam Kim <samuelkim523@gmail.com>
2018-12-07 17:25:20 -0500

    Fix results_parser issue with data_size

--- results/results_parser.py
+++ results/results_parser.py
@@ -2,13 +2,13 @@ import re
 
 # Create mapping from IP to Node
 NODE_MAP_STR = '''
-192.168.2.5 = NODE 1
-192.168.2.37 = NODE 2
-192.168.2.38 = NODE 3
-192.168.2.39 = NODE 5
-192.168.2.40 = NODE 6
-192.168.2.41 = NODE 4
-192.168.2.42 = NODE 7
+? (192.168.2.5) at 16:3a:43:2b:be:34 = NODE 1
+? (192.168.2.27) at 4e:3b:46:c6:6e:c8  = NODE 7
+? (192.168.2.28) at ae:5d:73:84:71:b6  = NODE 3
+? (192.168.2.29) at 6e:3c:19:9d:1b:74  = NODE 2
+? (192.168.2.30) at 8e:ab:87:93:93:d3  = NODE 4
+? (192.168.2.31) at 86:9:ed:95:2b:82  = NODE 5
+? (192.168.2.32) at a2:ca:e2:93:75:6b  = NODE 6
 '''
 def get_ip_to_node(input_str=NODE_MAP_STR):
     ip_to_node = {}
@@ -31,18 +31,18 @@ def get_ip_to_node(input_str=NODE_MAP_STR):
     return ip_to_node
 
 def get_results_one_trial(input_str='''
-[  7] local 192.168.2.5 port 8080 connected with 192.168.2.37 port 49378
-[  4] local 192.168.2.5 port 8080 connected with 192.168.2.38 port 48266
-[  5] local 192.168.2.5 port 8080 connected with 192.168.2.41 port 41190
-[  6] local 192.168.2.5 port 8080 connected with 192.168.2.42 port 40084
-[  9] local 192.168.2.5 port 8080 connected with 192.168.2.39 port 59236
-[  8] local 192.168.2.5 port 8080 connected with 192.168.2.40 port 45248
-[  9]  0.0-29.1 sec  5.40 MBytes  1.56 Mbits/sec
-[  5]  0.0-29.6 sec  8.89 MBytes  2.52 Mbits/sec
-[  6]  0.0-29.9 sec  6.45 MBytes  1.81 Mbits/sec
-[  8]  0.0-30.0 sec  8.30 MBytes  2.32 Mbits/sec
-[  7]  0.0-30.8 sec  12.5 MBytes  3.40 Mbits/sec
-[  4]  0.0-30.0 sec  8.93 MBytes  2.49 Mbits/sec
+[  9] local 192.168.2.5 port 8080 connected with 192.168.2.30 port 46330
+[  4] local 192.168.2.5 port 8080 connected with 192.168.2.29 port 38600
+[  5] local 192.168.2.5 port 8080 connected with 192.168.2.28 port 42118
+[  6] local 192.168.2.5 port 8080 connected with 192.168.2.32 port 51028
+[  7] local 192.168.2.5 port 8080 connected with 192.168.2.31 port 43472
+[  8] local 192.168.2.5 port 8080 connected with 192.168.2.27 port 49416
+[  4]  0.0-30.5 sec  28.6 MBytes  0.94 MBytes/sec <-- NODE 2
+[  5]  0.0-30.5 sec  25.0 MBytes  0.82 MBytes/sec <-- NODE 3
+[  9]  0.0-30.6 sec  15.9 MBytes  0.52 MBytes/sec <-- NODE 4
+[  7]  0.0-29.1 sec  18.7 MBytes  0.64 MBytes/sec <-- NODE 5
+[  6]  0.0-29.3 sec  5.50 MBytes  0.19 MBytes/sec <-- NODE 6
+[  8]  0.0-29.1 sec  6.97 MBytes  0.24 MBytes/sec <-- NODE 7
 '''):
     ip_to_node = get_ip_to_node() # Get IP to Node mapping
     trial_mapping = {} # Initialized in beginning
@@ -71,7 +71,7 @@ def get_results_one_trial(input_str='''
             data_units = data_units.replace('/sec', '')
             
             # Get data size
-            data_size = re.search(r'\d+ %s' % data_units, line).group()
+            data_size = re.search(r'[\d+\.]*\d %s' % data_units, line).group()
             data_size = data_size.replace(' %s' % data_units, '')
 
             # Get data speed
