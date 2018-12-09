dd8a3f3db2b22da32a15527b60899981681cc9a3
Sam Kim <samuelkim523@gmail.com>
2018-12-08 21:10:26 -0500

    Report diagrams, some data processing

---
+++ results/tdm-scheduled/get_average.py
@@ -0,0 +1,51 @@
+
+stream_1_throughputs = [float('39.13'),
+    float('38.0'),
+    float('46.0')]
+print('stream_1_throughputs avg = %.2f' % (sum(stream_1_throughputs) / len(stream_1_throughputs)))
+
+stream_2_throughputs = [float('212.5'),
+    float('157'),
+    float('124')]
+print('stream_2_throughputs avg = %.2f' % (sum(stream_2_throughputs) / len(stream_2_throughputs)))
+
+stream_3_throughputs = [float('186.25'),
+    float('112'),
+    float('199')]
+print('stream_3_throughputs avg = %.2f' % (sum(stream_3_throughputs) / len(stream_3_throughputs)))
+
+stream_4_throughputs = [float('43'),
+    float('112'),
+    float('76.1')]
+print('stream_4_throughputs avg = %.2f' % (sum(stream_4_throughputs) / len(stream_4_throughputs)))
+
+stream_5_throughputs = [float('9.51'),
+    float('39.7'),
+    float('1.16')]
+print('stream_5_throughputs avg = %.2f' % (sum(stream_5_throughputs) / len(stream_5_throughputs)))
+
+
+tdm_1_throughputs = [float('62.9'),
+    float('116'),
+    float('78.2')]
+print('tdm_1_throughputs avg = %.2f' % (sum(tdm_1_throughputs) / len(tdm_1_throughputs)))
+
+tdm_2_throughputs = [float('105.6'),
+    float('91.6'),
+    float('127')]
+print('tdm_2_throughputs avg = %.2f' % (sum(tdm_2_throughputs) / len(tdm_2_throughputs)))
+
+tdm_3_throughputs = [float('68.87'),
+    float('99.8'),
+    float('160')]
+print('tdm_3_throughputs avg = %.2f' % (sum(tdm_3_throughputs) / len(tdm_3_throughputs)))
+
+tdm_4_throughputs = [float('67.88'),
+    float('72.7'),
+    float('72.9')]
+print('tdm_4_throughputs avg = %.2f' % (sum(tdm_4_throughputs) / len(tdm_4_throughputs)))
+
+tdm_5_throughputs = [float('60.5'),
+    float('47.3'),
+    float('47.2')]
+print('tdm_5_throughputs avg = %.2f' % (sum(tdm_5_throughputs) / len(tdm_5_throughputs)))
